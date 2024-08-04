5226ffa7f9ba54004b54a3abd5a43f41ba875eab
JeongJaeSoon <jaesoon-jeong@freee.co.jp>
2024-08-04 16:23:05 +0900

    feat: Setup libes-server

--- libes-server/vite.config.ts
+++ libes-server/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Button': './src/shared/Button',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  build: {
+    target: 'esnext',
+  },
+});
